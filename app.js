3312db992a489a9a53c372b5700177ae9a0d3336
Shubham062004 <chaurasiaamrita400@gmail.com>
2023-10-23 09:45:42 +0530

    update js and html

---
+++ app.js
@@ -0,0 +1,120 @@
+// Virendra Sechwag
+let virendraphoto=document.getElementById("sehwag_photo")
+let virendra=document.getElementById("virendra")
+virendra.addEventListener("click",function(){
+    virendraphoto.style.display="block";
+    virendraphoto.style.transform="translate(5em,-20em)"
+    virendraphoto.onmouseleave=()=>{
+        virendraphoto.style.display="none";
+    }
+})
+
+//Sachin Tendulkar
+let sachinphoto=document.getElementById("sachin_photo")
+let sachin=document.getElementById("sachin")
+sachin.addEventListener("click",function(){
+    sachinphoto.style.display="block";
+    sachinphoto.style.transform="translate(5em,-18em)"
+    sachinphoto.onmouseleave=()=>{
+        sachinphoto.style.display="none";
+    }
+})
+
+//Gautam Gambir
+let gautamphoto=document.getElementById("gambhir_photo")
+let gautam=document.getElementById("gautam")
+gautam.addEventListener("click",function(){
+    gautamphoto.style.display="block";
+    gautamphoto.style.transform="translate(5em,-16em)"
+    gautamphoto.onmouseleave=()=>{
+        gautamphoto.style.display="none";
+    }
+})
+
+//Virat Kohli
+let viratphoto=document.getElementById("virat_photo")
+let virat=document.getElementById("virat")
+virat.addEventListener("click",function(){
+    viratphoto.style.display="block";
+    viratphoto.style.transform="translate(4em,-14em)"
+    viratphoto.onmouseleave=()=>{
+        viratphoto.style.display="none";
+    }
+})
+
+//MS Dhoni
+let dhoniphoto=document.getElementById("dhoni_photo")
+let dhoni=document.getElementById("dhoni")
+dhoni.addEventListener("click",function(){
+    dhoniphoto.style.display="block";
+    dhoniphoto.style.transform="translate(5em,-12em)"
+    dhoniphoto.onmouseleave=()=>{
+        dhoniphoto.style.display="none";
+    }
+})
+
+//Yuvraj Singh
+let yuvrajphoto=document.getElementById("yuvraj_photo")
+let yuvraj=document.getElementById("yuvraj")
+yuvraj.addEventListener("click",function(){
+    yuvrajphoto.style.display="block";
+    yuvrajphoto.style.transform="translate(5em,-10em)"
+    yuvrajphoto.onmouseleave=()=>{
+        yuvrajphoto.style.display="none";
+    }
+})
+
+//Suresh Raina
+let sureshphoto=document.getElementById("raina_photo")
+let suresh=document.getElementById("raina")
+suresh.addEventListener("click",function(){
+    sureshphoto.style.display="block";
+    sureshphoto.style.transform="translate(5em,-10em)"
+    sureshphoto.onmouseleave=()=>{
+        sureshphoto.style.display="none";
+    }
+})
+
+//Harbhajan Singh
+let bhajanphoto=document.getElementById("bhajan_photo")
+let bhajan=document.getElementById("singh")
+bhajan.addEventListener("click",function(){
+    bhajanphoto.style.display="block";
+    bhajanphoto.style.transform="translate(5em,-10em)"
+    bhajanphoto.onmouseleave=()=>{
+        bhajanphoto.style.display="none";
+    }
+})
+
+// Zaheer Khan
+let  zaheerphoto=document.getElementById("khan_photo")
+let zaheer=document.getElementById("khan")
+zaheer.addEventListener("click",function(){
+    zaheerphoto.style.display="block";
+    zaheerphoto.style.transform="translate(5em,-10em)"
+    zaheerphoto.onmouseleave=()=>{
+        zaheerphoto.style.display="none";
+    }
+})
+
+//Munaf Patel
+let munafphoto=document.getElementById("patel_photo")
+let munaf=document.getElementById("patel")
+munaf.addEventListener("click",function(){
+    munafphoto.style.display="block";
+    munafphoto.style.transform="translate(5em,-10em)"
+    munafphoto.onmouseleave=()=>{
+        munafphoto.style.display="none";
+    }
+})
+
+//Sreeshanth
+let sreeshanthphoto=document.getElementById("shanth_photo")
+let sreeshanth=document.getElementById("shanth")
+sreeshanth.addEventListener("click",function(){
+    sreeshanthphoto.style.display="block";
+    sreeshanthphoto.style.transform="translate(5em,-10em)"
+    sreeshanthphoto.onmouseleave=()=>{
+        sreeshanthphoto.style.display="none";
+    }
+})
